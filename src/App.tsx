53d2d9c55193552db75efa05f2005cc7a09a767b
unknown <fabiolimadiogenes@gmail.com>
2021-06-23 15:53:30 -0300

    Finalização da aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
+
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+   
+    <BrowserRouter>
+
+      <AuthContextProvider>
+      {/* se o path for vazio o exact não precisa de parametro */}
+        <Route path="/" exact component={Home} /> {/*exact verifica se o que foi passado no path é exatamente o que foi escrito. Do contrário ele procuraria se inicia com o que foi passado*/}
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+      
   );
 }
 
